4a0ef4d493090d652b8d964f0b98621e888ec7c7
Jonathan Herrmann-Engel <38504994+jonathanherrmannengel@users.noreply.github.com>
2021-10-16 12:18:27 +0200

    7.2.5

--- moroway-app-oc/src/js/appdata.js
+++ moroway-app-oc/src/js/appdata.js
@@ -3,12 +3,12 @@ const APP_DATA = {
     "version": {
         "major": 7,
         "minor": 2,
-        "patch": 4,
+        "patch": 5,
         "beta": 0,
         "date": {
             "year": 2021,
             "month": 10,
-            "day": 6
+            "day": 16
         }
     },
     "platform": "oc",
